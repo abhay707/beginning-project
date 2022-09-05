cd908acd621e45376b2619d0b383c30fa8f06209
abhay707 <109450662+abhay707@users.noreply.github.com>
2022-09-05 23:07:13 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,19 @@
+#swapping
+#by taking third variable
+a,b = 30,40
+temp = a
+a = b
+b = temp
+print("value of a: ",a,"\nvalue of b :",b)
+#by addition subtracton
+a,b = 25,30
+a = a + b
+b = a - b
+a = a - b
+print("value of a: ",a,"\nvalue of b :",b)
+#by division and miltiplication
+a , b = 29,45
+a = a*b
+b = a//b
+a = a//b
+print("value of a: ",a,"\nvalue of b :",b)
